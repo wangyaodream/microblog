cf98b54456ee0f2addcd13647ef7413bf067129b
wangyao <wangyao.dream@hotmail.com>
2019-07-03 23:14:25 +0800

    add templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world！"
+    user = {'username': 'wangyao'}
+    return render_template('index.html', user=user)
+
+
+@app.route('/loop')
+def loop_index():
+    user = {'username': 'wangyao'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('loop_index.html', title='Home', user=user, posts=posts)
